68b68d72699cdc1224a9702d8a6983a2a658a440
Andrew Weil <weilandrew@icloud.com>
2023-09-12 19:57:21 -0400

    clearing up full meal plan. Output not displaying

--- FYM.py
+++ FYM.py
@@ -41,6 +41,8 @@ def recipeGeneration():
     print(recipe)
     return str(recipe)
 
+
+
 @app.route('/recipeGenerationFullDay', methods=['POST'])
 def recipeGenerationFullDay():
     protein = request.form.get('protein')
@@ -57,13 +59,12 @@ def recipeGenerationFullDay():
     openai.organization = ORG_KEY
     openai.api_key = API_KEY
 
-    prompt = "For the response I would like a full day of eating that satisfies the given information. In the full day I want there to be " + protein + " grams of protein"
+    prompt = "For the response I would like a full day of eating that satisfies the given information. In the full day I want there to be " + protein + " grams of protein, " + carb + " grams of carbs, " + fat + " grams of fat. At around a total of " + calorie + " calories for the day. Along with this incorporate the following information for the meal plan: " + otherInfo + ". In the response begin with how many meals there are, the macro nutrient count in each meal and then the keep each recipe organized. "
     print("Running AI")
     completion = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "Give me a recipe that has around " + protein + " grams of protein, " + carb + " grams of carbohydrate, " +
-                fat + " grams of fat and has " + calorie + " calories. Along with that include this information: " + otherInfo + " For the Response please first calculate and list the macronutrients and calorie count. After that make 2 new lines then  list of ingredents followed by 3  new lines and then  the cooking instructions."}
+            {"role": "system", "content": prompt}
         ]
     )
     print("Done with AI call")
