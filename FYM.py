77f4550b4ed1f633ef70c1e581ef489fd232cb50
aweil5 <aweil@terpmail.umd.edu>
2023-09-18 20:19:17 -0400

    changing promptnig

--- FYM.py
+++ FYM.py
@@ -19,6 +19,30 @@ def recipeGeneration():
     fat = request.form.get('fat')
     calorie = request.form.get('calorie')
     otherInfo = request.form.get('otherInfo')
+
+    prompt = ". In the response begin with the recipe name, the macro nutrient count in the meal and then the ingredients to make it followed by the steps to make the recipe."
+
+    prompt = prompt + """ Create a RFC8259 compliant JSON response  following this format without deviation:
+    [{
+        "RecipeName": "The name of the recipe",
+        "Macronutrients": "The macronutrients of this recipe",
+        "Ingredients":"The ingredients of the recipe",
+        "Steps": "The Steps to make the recipe
+
+    }]"""
+
+    if(len(protein) != 0):
+        prompt = "I want there to be " + protein  + " grams of protein." + prompt
+    if(len(carb) != 0):
+        prompt = "I want there to be " + carb  + " grams of carbohydrates." + prompt
+    if(len(fat) != 0):
+        prompt = "I want there to be " + fat  + " grams of fats." + prompt
+    if(len(calorie) != 0):
+        prompt = "I want there to be " + calorie  + " calories." + prompt
+    if(len(otherInfo) != 0):
+        prompt = "Along with this incorporate the following information for the meal plan: " + otherInfo + prompt
+
+    prompt = "Please respond with a recipe satsifies the following information and include  the macro nutrients for the recipe, its required ingredients and the steps to make them. " + prompt
     
     load_dotenv()
 
@@ -31,8 +55,7 @@ def recipeGeneration():
     completion = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "Give me a recipe that has around " + protein + " grams of protein, " + carb + " grams of carbohydrate, " +
-                fat + " grams of fat and has " + calorie + " calories. Along with that include this information: " + otherInfo + " For the Response please first calculate and list the macronutrients and calorie count. After that make 2 new lines then  list of ingredents followed by 3  new lines and then  the cooking instructions."}
+            {"role": "system", "content": prompt}
         ]
     )
     print("Done with AI call")
@@ -50,7 +73,23 @@ def recipeGenerationFullDay():
     fat = request.form.get('fat')
     calorie = request.form.get('calorie')
     otherInfo = request.form.get('otherInfo')
+
+    prompt = ". In the response begin with how many meals there are, the macro nutrient count in each meal and then list each recipe in order, first being the recipe name, then the macronutrients, then the ingredients, then the steps to make it."
+
+    if(len(protein) != 0):
+        prompt = "I want there to be " + protein  + " grams of protein." + prompt
+    if(len(carb) != 0):
+        prompt = "I want there to be " + carb  + " grams of carbohydrates." + prompt
+    if(len(fat) != 0):
+        prompt = "I want there to be " + fat  + " grams of fats." + prompt
+    if(len(calorie) != 0):
+        prompt = "I want there to be " + calorie  + " calories." + prompt
+    if(len(otherInfo) != 0):
+        prompt = "Along with this incorporate the following information for the meal plan: " + otherInfo + prompt
+
+    prompt = "Please respond with a meal plan for a day of eating that satsifies the following information and include each recipe for the day, the macro nutrients for each recipe, their required ingredients and the steps to make them. " + prompt
     
+
     load_dotenv()
 
     API_KEY = os.getenv("OPENAI_API_KEY")
